905e47b995d96cfa7c177960456a493147232601
roihas1 <103654352+roihas1@users.noreply.github.com>
2023-04-24 18:08:05 +0300

    cosmetic update

--- starships.js
+++ starships.js
@@ -11,7 +11,6 @@ var startTime;
 var time_elapes = 0;
 var counter_update_speed=0;
 var game_started = false;
-var lineWidth;
 var liveRemaining =3;
 var ROWS = 4
 var timeLeft;
@@ -23,17 +22,7 @@ var enemyballOnScreen;
 var intervalTimer;
 var COLUMN = 5;
 var canvasWidth;
-var canvasHeight;
-var cannonBaseRadius;
-var cannonLength;
-var cannonballRadius;
 var cannonballSpeed;
-var targetDistance;
-var targetBeginning;
-var targetEnd;
-var pieceLength;
-var initialTargetVelocity;
-var barrelEnd;
 var ship;
 var SHOT=32;
 var myShot;
@@ -43,7 +32,7 @@ var points=0;
 var enemiesVelocity;
 var radius = 20;
 var enemyShots;
-var enemieskilled=0;
+// var enemieskilled=0;
 var record_history;
 var shot_interval;
 var speed_enemies_interval;
@@ -64,7 +53,6 @@ var context;
 var canvas;
 var requestID;
 var started=false;
-
 var TARGET_PIECES = ROWS * COLUMN;//number of enemy ships
 
 function openDialog() {
@@ -258,7 +246,6 @@ function setupGame() {
 
     // JavaScript Object representing game items
     shipball = new Object(); // object representing cannonball point
-    barrelEnd = new Object(); // object representing end of cannon barrel
     ship = new Object();
     myShot=new Object();
     enemyShots = [];
@@ -328,8 +315,6 @@ function newGame() {
     draw();
     
 
-    targetPiecesHit = 0; // no target pieces have been hit
-    targetVelocity = initialTargetVelocity; // set initial velocity
     timeLeft = INITIAL_GAME_TIME; // start the countdown at 10 seconds
     timerCount = 0; // the timer has fired 0 times so far
     enemyballOnScreen = false; // the enemyball is not on the screen
@@ -401,9 +386,7 @@ function stopTimer() {
 // relative to the size of the canvas before the game begins
 
 function resetElements() {
-    // if(started){
-    //     stopTimer(); // terminate previous interval timer
-    // }
+   
     
     context.clearRect(0,0,canvas.width,canvas.height); // clear canvas
     startTime = new Date();
@@ -412,26 +395,10 @@ function resetElements() {
     enemiesVelocity = 1.5;
     counter_update_speed = 0;
     cannonballOnScreen=false;
-    var w = canvas.width;
-    var h = canvas.height;
-    canvasWidth = w; // store the width
-    canvasHeight = h; // store the height
-    cannonBaseRadius = h / 18; // cannon base radius 1/18 canvas height
-    cannonLength = w / 8; // cannon length 1/8 canvas width
-
-    cannonballRadius = w / 36; // cannonball radius 1/36 canvas width
+    var w = canvas.width; // width of canvas
+    canvasWidth = w; // store the width of the canvas
     cannonballSpeed = w / 2; // cannonball speed multiplier
 
-    // configure instance variables related to the target
-    targetDistance = w * 7 / 8; // target 7/8 canvas width from left
-    targetBeginning = h / 8; // distance from top 1/8 canvas height
-    targetEnd = h * 7 / 8; // distance from top 7/8 canvas height
-    pieceLength = (targetEnd - targetBeginning) / TARGET_PIECES;
-    initialTargetVelocity = -h / 4; // initial target speed multiplier
-
-    // end point of the cannon's barrel initially points horizontally
-    barrelEnd.x = cannonLength;
-    barrelEnd.y = h / 2;
     
 } // end function resetElements
 
@@ -443,11 +410,6 @@ function draw(){
     if (cannonballOnScreen) {
         context.drawImage(myShotImg,myShot.x,myShot.y,10,30);
   
-        // context.fillStyle = "white";
-        // context.beginPath();
-        // context.arc(myShot.x,myShot.y,SHOT_RADIUS,0,Math.PI *2);
-        // // context.closePath();
-        // context.fill();
     }
     context.strokeStyle="black";
     context.lineWidth=2;
@@ -466,14 +428,7 @@ function draw(){
     var minuts = Math.floor(time_remained/60);
     div.innerText = "Time Left: " + minuts +":"+seconds +" \n\nScore: " + points + " \n\nLives: " + liveRemaining;
 
-//    context.fillText("Time remaining: " + time_remained, 5, 5);
 
-//       // display points 
-//     context.fillText("Score: " + points, 5, 25);
-
-//     // display live
-//     context.fillText('Lives: ' + liveRemaining,300,5)
-// display enemy shots
     drawShots();
 
    context.drawImage(shipImg,ship.x,ship.y,60,60);
@@ -481,29 +436,30 @@ function draw(){
 
     const button = document.getElementById("start");
     button.value = "Restart";
-for (let i = 0; i < ROWS; i++) {
-  for (let j = 0; j < COLUMN; j++) {
-    if(!hitStates[i][j].hit){
-    //     context.beginPath();
-    // context.arc(hitStates[i][j].x,hitStates[i][j].y, radius, 0, 2 * Math.PI);
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLUMN; j++) {
+            if(!hitStates[i][j].hit){
+            //     context.beginPath();
+            // context.arc(hitStates[i][j].x,hitStates[i][j].y, radius, 0, 2 * Math.PI);
 
-    if(i===0)
-        // context.fillStyle = 'blue';
-        context.drawImage(shipenemyImg1,hitStates[i][j].x,hitStates[i][j].y,60,60);
-    if(i===1)
-        // context.fillStyle = 'yellow';
-        context.drawImage(shipenemyImg2,hitStates[i][j].x,hitStates[i][j].y,60,60);
-    if(i===2)
-        // context.fillStyle = 'red';
-        context.drawImage(shipenemyImg3,hitStates[i][j].x,hitStates[i][j].y,60,60);
-    if(i===3)
-        // context.fillStyle = 'green';
-        context.drawImage(shipenemyImg4,hitStates[i][j].x,hitStates[i][j].y,60,60); 
-    // context.fill();   
-  }
-}
-}
-}
+            if(i===0)
+                // context.fillStyle = 'blue';
+                context.drawImage(shipenemyImg1,hitStates[i][j].x,hitStates[i][j].y,60,60);
+            if(i===1)
+                // context.fillStyle = 'yellow';
+                context.drawImage(shipenemyImg2,hitStates[i][j].x,hitStates[i][j].y,60,60);
+            if(i===2)
+                // context.fillStyle = 'red';
+                context.drawImage(shipenemyImg3,hitStates[i][j].x,hitStates[i][j].y,60,60);
+            if(i===3)
+                // context.fillStyle = 'green';
+                context.drawImage(shipenemyImg4,hitStates[i][j].x,hitStates[i][j].y,60,60); 
+            // context.fill();   
+            }
+        }   
+    }
+}//end draw function
+
 function updateLeaderboard() {
     // assume scores and names are retrieved from a database or other data source
     try{
@@ -529,10 +485,7 @@ function updateLeaderboard() {
     // Create header cells and add them to the header row
     for (let i = 0; i < headerCells.length; i++) {
       let cell = document.createElement('th');
-    //   cell.style.fontWeight = 'bold';
-    //   cell.style.textDecoration = 'underline';
       cell.style.borderBottom = '5px solid ';
-
       cell.innerHTML = headerCells[i];
       headerRow.appendChild(cell);
     }
@@ -546,7 +499,6 @@ function updateLeaderboard() {
     for (let i = 0; i < dataRows.length; i++) {
       let row = document.createElement('tr');
       row.style.borderTop = '2px solid';
-
       let cell1 = document.createElement('td');
       cell1.innerHTML = dataRows[i].toString();
       let cell2 = document.createElement('td');
@@ -559,7 +511,7 @@ function updateLeaderboard() {
     let container = document.querySelector('#after_game');
     container.appendChild(table);
 
-}
+}//end updateLeaderboard function
 
 
 function updateShot(){
@@ -586,18 +538,16 @@ function updateShot(){
     
     }
 
-}
+}//end updateShot function
+
 function drawShots(){
-        for (var i=0;i<enemyShots.length;i++){
-            context.drawImage(enemyShotImg,enemyShots[i].x,enemyShots[i].y,10,30);
-            // context.fillStyle = "black";
-            // context.beginPath();
-            // context.arc(enemyShots[i].x, enemyShots[i].y, SHOT_RADIUS, 0, Math.PI *2);
-            // context.fill();
+    for (var i=0;i<enemyShots.length;i++){
+        context.drawImage(enemyShotImg,enemyShots[i].x,enemyShots[i].y,10,30);
             
-        }
+    }
        
-}
+}//end drawShots function
+
 function enemyShoot(){
     
     var randomRow = Math.floor(Math.random() *(ROWS));
@@ -609,9 +559,8 @@ function enemyShoot(){
     var shot = {x:hitStates[randomRow][randomColumn].x, y:hitStates[randomRow][randomColumn].y}
     if(enemyShots.length < 2)
         enemyShots.push(shot)
+}//end enemyShoot function
 
-
-}
 function display_records(message){
     GameMusic.pause();
     GameMusic.currentTime = 0;
@@ -619,54 +568,17 @@ function display_records(message){
     for (var i=0; i<record_history.records.length;i++){
         str+=record_history.records[i]+"\n";
     }
-    // alert(message);
     var div = document.getElementById("endGame");
     div.innerHTML = message;
     document.getElementById("canvasGame").style.display = "none";
     document.getElementById("after_game").style.display = "block";
     updateLeaderboard();
     resetElements();
-}
+}//end display_records function
+
 function UpdatePosition() {
     var x = GetKeyPressed();
 
-    if(liveRemaining==0){
-        youLostMusic.currentTime = 0;
-        youLostMusic.muted = false;
-        youLostMusic.play();
-        stopTimer();
-        record_history.records.push(points);
-        display_records("You Lost");
-
-    }
-    if(points==250){
-        youWonMusic.currentTime = 0;
-        youWonMusic.muted = false;
-        youWonMusic.play();
-        stopTimer();
-        record_history.records.push(points);
-        display_records("Champion!");
-        
-    }
-    if(INITIAL_GAME_TIME-time_elapes<=0.5){
-        if(points<100){
-            youLostMusic.currentTime = 0;
-            youLostMusic.muted = false;
-            youLostMusic.play();
-            record_history.records.push(points);
-            stopTimer();
-            display_records("you can do better");
-            
-        }
-        else{
-            youWonMusic.currentTime = 0;
-            youWonMusic.muted = false;
-            youWonMusic.play();
-            record_history.records.push(points);
-            stopTimer();
-            display_records("winner");
-        }
-    }
     if(x==1)//up
     {
         if(ship.y>=canvas.height*(2/3))
@@ -705,27 +617,19 @@ function UpdatePosition() {
         }  
 
     }
+    update_direction_enemy();
 
-        for(var i=0;i<ROWS;i++){
-            for(var j=0;j<COLUMN;j++){
-                hitStates[i][j].x += enemiesVelocity;
-                if(!hitStates[i][j].hit){
-                if(hitStates[i][j].x>canvas.width*(14/15)){
-                    hitStates[i][j].x = canvas.width*(14/15);
-                    enemiesVelocity*=-1;
-                    updateEnemyPosition("right");
-                }
-                if(hitStates[i][j].x<canvas.width*(1/20)){
-                    hitStates[i][j].x=canvas.width*(1/20);
-                    enemiesVelocity*=-1;
-                    updateEnemyPosition("left");
-                }
-            }
-            }
-        }
     
     var nowTime = new Date();
-    time_elapes = (nowTime-startTime)/1000;    
+    time_elapes = (nowTime-startTime)/1000; 
+    update_ship_shot();   
+    
+    end_game();
+    draw();    
+}//end UpdatePosition function
+
+function update_ship_shot(){
+    //this function update the ship shot y position
     var to_brake=false;
     if(cannonballOnScreen){
         var interval = TIME_INTERVAL /1000.0;
@@ -736,7 +640,7 @@ function UpdatePosition() {
                     if(myShot.x >= hitStates[i][j].x-30 && myShot.x <= hitStates[i][j].x+30 && 
                         myShot.y >= hitStates[i][j].y-30 && myShot.y <= hitStates[i][j].y+30 ){
                             hitStates[i][j].hit=true;
-                            enemieskilled++;
+                            // enemieskilled++;
                             points+=20-i*5;
                             cannonballOnScreen=false;
                             to_brake=true;
@@ -755,11 +659,66 @@ function UpdatePosition() {
         {
             cannonballOnScreen = false;
         }  
+}// end update_ship_shot function
+function update_direction_enemy(){
+    // this function update the enemy direction
+    for(var i=0;i<ROWS;i++){
+        for(var j=0;j<COLUMN;j++){
+            hitStates[i][j].x += enemiesVelocity;
+            if(!hitStates[i][j].hit){
+                if(hitStates[i][j].x>canvas.width*(14/15)){
+                    hitStates[i][j].x = canvas.width*(14/15);
+                    enemiesVelocity*=-1;
+                    updateEnemyPosition("right");
+                }
+                if(hitStates[i][j].x<canvas.width*(1/20)){
+                    hitStates[i][j].x=canvas.width*(1/20);
+                    enemiesVelocity*=-1;
+                    updateEnemyPosition("left");
+                }
+            }
+        }
+    }
+}
+function end_game(){
+    // check if the game is over for several reasons
+    if(liveRemaining==0){
+        youLostMusic.currentTime = 0;
+        youLostMusic.muted = false;
+        youLostMusic.play();
+        stopTimer();
+        record_history.records.push(points);
+        display_records("You Lost");
 
-  
-
-    
-    draw();    
+    }
+    if(points==250){
+        youWonMusic.currentTime = 0;
+        youWonMusic.muted = false;
+        youWonMusic.play();
+        stopTimer();
+        record_history.records.push(points);
+        display_records("Champion!");
+        
+    }
+    if(INITIAL_GAME_TIME-time_elapes<=0.5){
+        if(points<100){
+            youLostMusic.currentTime = 0;
+            youLostMusic.muted = false;
+            youLostMusic.play();
+            record_history.records.push(points);
+            stopTimer();
+            display_records("you can do better");
+            
+        }
+        else{
+            youWonMusic.currentTime = 0;
+            youWonMusic.muted = false;
+            youWonMusic.play();
+            record_history.records.push(points);
+            stopTimer();
+            display_records("winner");
+        }
+    }
 }
 function updateEnemyPosition(direction){
     var gap=100;
@@ -782,49 +741,21 @@ function updateEnemyPosition(direction){
 
     }
 }
-//     if(direction==="right"){
-//         for (let i = ROWS-1; i >=0; i--) {
-//          for (let j = COLUMN-1; j >= 0; j--) {
-//             if(!hitStates[i][j].hit){
-//              hitStates[i][j].x=Math.min(hitStates[i][j].x+80,canvas.width*(14/15));
-//             }
-//          }
-//      }
-//     }
-//     else{
-//         for (let i = 0; i <ROWS; i++) {
-//          for (let j = 0; j <COLUMN; j++) {
-//             if(!hitStates[i][j].hit){
-//                 hitStates[i][j].x=Math.max(hitStates[i][j].x-80,canvas.width*(1/15));
-//             }
-//          }
 
-//     }
-// }
 
 
 }
 function ShipShot(){
-    // if (cannonballOnScreen) // if a cannonball is already on the screen
-    // return; // do nothing
-
- // move the cannonball to be inside the cannonS
- ShipShotSound.muted = false;
-ShipShotSound.currentTime = 0;
-ShipShotSound.play();
- myShot.x = ship.x;
- myShot.y = ship.y;
-
- // get the x component of the total velocity
- // get the y component of the total velocity
- cannonballVelocityY = (-cannonballSpeed);
- cannonballOnScreen = true; // the cannonball is on the screen
-//  ++shotsFired; // increment shotsFired
-
- // play cannon fired sound
-//  cannonSound.play();
-
-}
+    // move the cannonball to be inside the cannonS
+    ShipShotSound.muted = false;
+    ShipShotSound.currentTime = 0;
+    ShipShotSound.play();
+    myShot.x = ship.x;
+    myShot.y = ship.y;
+    cannonballVelocityY = (-cannonballSpeed);
+    cannonballOnScreen = true; // the cannonball is on the screen
+    
+}//end ShipShot function
 window.addEventListener("load", setupGame, false);
 
 // window.addEventListener("keydown", UpdatePosition, false);
